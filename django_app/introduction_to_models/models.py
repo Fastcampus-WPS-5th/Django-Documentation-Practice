9abbe6d8a53b7171c71005b3a8288412a40fc9a4
Lee <dev@azelf.com>
2017-06-05 12:50:43 +0900

    Person모델 수정(choices, recursive foreign key추가), Car, Manufacturer모델 추가

--- django_app/introduction_to_models/models.py
+++ django_app/introduction_to_models/models.py
@@ -8,8 +8,35 @@ class Person(models.Model):
         ('M', 'Medium'),
         ('L', 'Large'),
     )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    PERSON_TYPES = (
+        ('student', '학생'),
+        ('teacher', '선생'),
+    )
+
+    person_type = models.CharField(
+        '유형',
+        max_length=10,
+        choices=PERSON_TYPES,
+        default=PERSON_TYPES[0][0]
+    )
+    # teacher속성 지정 (ForeignKey, 'self'를 이용해 자기 자신을 가리킴, null=True허용)
+    teacher = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField('이름', max_length=60)
+    shirt_size = models.CharField(
+        '셔츠사이즈',
+        max_length=1,
+        choices=SHIRT_SIZES,
+        help_text='남자는 L쓰세요',
+    )
+
+    def __str__(self):
+        return self.name
+
 
 # INSTALLED_APPS에 이 모델이 속하는 app추가
 # makemigrations로 migrations생성
@@ -17,3 +44,23 @@ class Person(models.Model):
 # admin.py에 Person클래스 등록
 # createsuperuser로 슈퍼유저 계정 생성
 # runserver후 admin접속해서 Person객체 생성 및 저장해보기
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=40)
+    manufacturer = models.ForeignKey(
+        # Manufacturer,
+        # 'introduction_to_models.Manufacturer'
+        'Manufacturer',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
