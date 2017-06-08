95c1f9e15e32e63c17adc672c612bb92bb55c926
Lee <dev@azelf.com>
2017-06-08 14:07:47 +0900

    MTM필드 -> Intermediate로 전환할때 마이그레이션 관리

--- django_app/introduction_to_models/models/mixin.py
+++ django_app/introduction_to_models/models/mixin.py
@@ -24,23 +24,42 @@ from django.db import models
 from utils.models.mixins import TimeStampedMixin
 
 
-class Post(TimeStampedMixin):
-    author = models.ForeignKey('User')
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-
-
-class Comment(TimeStampedMixin):
-    post = models.ForeignKey(Post)
-    author = models.ForeignKey('User')
-    content = models.TextField()
-
-
 class User(TimeStampedMixin):
     name = models.CharField(max_length=50)
 
 
+class Post(TimeStampedMixin):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    like_users = models.ManyToManyField(
+        User,
+        related_name='like_posts',
+        through='PostLike',
+    )
+
+
+class Comment(TimeStampedMixin):
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(User)
+    content = models.TextField()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     db_table = 'introduction_to_models_post_like_users'
+
+
 """
+1. PostLike, Tag모델 추가
+2. Post모델에 like_users MTM필드 추가 및 through선언
+3. makemigrations -> migrate잘 되는지 확인
+4. Django shell에서 Post의 like_users에 임의의 User추가하고 결과 확인
+
 PostLike
     post = Post
     user = User
@@ -55,4 +74,7 @@ Post모델
     
     def like_post(self, user):
         return '해당 user의 PostLike를 생성, 이후 생성 객체를 리턴'
+        
+    def add_tag(self, tag_name):
+        return '해당 tag_name의 Tag를 생성 또는 기존항목 가져와서 Post에 추가, 이후 Tag리턴'
 """
